f95f7008e3a656651920af5cc7bd9c8d0819f2d7
pumpatta <jliljavirta@gmail.com>
2018-02-13 13:11:40 +0200

    UserInterface ensimmäiset käskyt

---
+++ src/UserInterface.java
@@ -0,0 +1,32 @@
+public class UserInterface {
+
+
+    public String go() {
+        return "GO";
+    }
+
+    public String use() {
+        return "USE";
+    }
+
+    public String take() {
+        return "TAKE";
+    }
+
+    public String search() {
+        return "SEARCH";
+    }
+
+    public String open() {
+        return "OPEN";
+    }
+
+    public String help(){
+        return "Usable commands:\nGO\nUSE\nTAKE\nSEARCH\nOPEN\nINVENTORY\nHELP";
+    }
+
+    public String actionTarget() {
+        return null;
+    }
+
+}
