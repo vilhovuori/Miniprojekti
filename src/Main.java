9b536b6726b592e5dd8a1c55c8a6ff03dd1f6b99
vilhovuori <vilhovuori@gmail.com>
2018-02-13 14:00:58 +0200

    Eka testi onnistunut, Main kasvatettu action

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
 import java.util.Scanner;
 
 public class Main {
+
+
+
+    Room office = new Room("toimisto", 2, "The office, or so you at least think", new int[]{-1,-1,3,1});
+    Room meeting = new Room("meeting", 3, "The meeting, or so you at least think", new int[]{2,-1,-1,-1});
+    int position = office.getRoomNumber();
+
+
     public static void main(String[] args) {
+        UserInterface ui = new UserInterface();
 
     Scanner scanner = new Scanner (System.in);
     Player player = new Player ();
@@ -11,7 +20,13 @@ public class Main {
 //    player.setPlayerName (name);
 
         System.out.println("What you wish to do");
-        scanner.nextLine();
+        String komento = scanner.nextLine();
+
+        System.out.println(ui.action(komento));
+
+
+
+
 
 
 
