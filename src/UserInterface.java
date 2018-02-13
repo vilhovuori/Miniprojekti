911e3895abfdb1c6e771ad46dd81363815d4230f
pumpatta <jliljavirta@gmail.com>
2018-02-13 15:24:58 +0200

    Go-metodin päivitys

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,11 +1,33 @@
+import java.util.Map;
+
 public class UserInterface {
 
-    public String action(String command){
-        return help();
+    public String action(String command) {
+        if (command == "GO") {
+            return go();
+        }
+        return "virhetilanne";
     }
 
-    public String go() {
-        return "GO";
+    public int go(String direction) {
+        int d;
+        switch (direction) {
+            case "NORTH":
+                d = 0;
+                break;
+            case "EAST":
+                d = 1;
+                break;
+            case "SOUTH":
+                d = 2;
+                break;
+            case "WEST":
+                d = 3;
+                break;
+            default:
+                d = -1;
+        }
+        return d;
     }
 
     public String use() {
@@ -24,7 +46,7 @@ public class UserInterface {
         return "OPEN";
     }
 
-    public String help(){
+    public String help() {
         return "Usable commands:\nGO\nUSE\nTAKE\nSEARCH\nOPEN\nINVENTORY\nHELP";
     }
 
