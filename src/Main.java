2537cb3aca70e6b4df2c4f133d42643b3b8d338a
TiinaEn <tiina.enroth@outlook.com>
2018-02-14 16:30:08 +0200

    Kirjeeseen lisätty teksti: ohjeet hyvään kahviin

--- src/Main.java
+++ src/Main.java
@@ -108,9 +108,11 @@ public class Main {
                            System.out.println("You already have this item");
                         } else {
                             System.out.println("You picked up the " + otettuEsine);
-
                             Item.inventory.add(otettuEsine);
                             System.out.println(Item.inventory);
+                                if (otettuEsine.equals("letter")) {
+                                    System.out.println("Instructions for good coffee: take some fresh water and aromatic coffee beans.");
+                                }
                         }
                 } else {
                     System.out.println("Your command does not make any sense. Try again.");
