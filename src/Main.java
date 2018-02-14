8d1d06e9c9d945567729db15e0531314cefd6709
pumpatta <jliljavirta@gmail.com>
2018-02-14 16:18:06 +0200

    Game over teksti tehty. Peli päättyy jos kuolee.

--- src/Main.java
+++ src/Main.java
@@ -143,7 +143,7 @@ public class Main {
                     System.out.println("Your HP:" + playerHealth + " " + "Coffee zombie's HP:" +zombieHealth);
                     String taisteluToiminto = scanner.nextLine().toUpperCase();
                     if (Item.inventory.contains(knife)) {
-                        playerDamage += 5;}
+                        playerDamage += 10;}
                     if (taisteluToiminto.matches("KNIFE|FISTS")){
                         zombieHealth -= playerDamage;
                         playerHealth -= zombieDamage;
