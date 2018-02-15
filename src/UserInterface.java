c7c1d658dd410862f311af75aa2c2148646974ec
pumpatta <jliljavirta@gmail.com>
2018-02-15 12:50:03 +0200

    Use aloitettu

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -38,24 +38,36 @@ public class UserInterface {
                 break;
             case "KEY":
                 u = "You opened the door.";
-               Main.building.get(3).setDirections(new int[]{2, -1, -1, 4});
-               Main.building.get(4).setDirections(new int[]{1, 3, -1, -1});
+                Main.building.get(3).setDirections(new int[]{2, -1, -1, 4});
+                Main.building.get(4).setDirections(new int[]{1, 3, -1, -1});
+                String s = Main.building.get(3).getDescription();
+                String replaceString = s.replace("The western door is locked. ", "");
+                Main.building.get(3).setDescription(replaceString);
+                String s2 = Main.building.get(4).getDescription();
+                String replaceString2 = s2.replace("locked", "unlocked");
+                Main.building.get(4).setDescription(replaceString2);
                 break;
             case "COFFEE":
                 u = "You put the coffee in the coffee machine.";
-                String s1=Main.building.get(5).getDescription();
-                String replaceString=s1.replace(" A letter is placed on the reception desk.","");
-                Main.building.get(5).setDescription(replaceString);
+                Juuso.coffeeMachineStatus = Juuso.coffeeMachineStatus + 1;
+                Item.inventory.remove("COFFEE");
+
                 break;
             case "ELECTRICWIRE":
                 u = "The coffee machine is turned on.";
-                Main.building.get(4).setDescription("You are in the Classroom. There's a door to the north and a locked door to the east. A coffee maker stands in the corner, and it is powered. \" +\n" + "In the other corner there's a thing that resembles a coffee bag with the label: \\\"KORVIKE-COFFEE\\\".");
+                Juuso.coffeeMachineStatus = Juuso.coffeeMachineStatus + 1;
+                String s3 = Main.building.get(4).getDescription();
+                String replaceString3 = s3.replace("A coffee maker stands in the corner, but there's no electric wire.", "A powered coffee maker stands in the corner.");
+                Main.building.get(4).setDescription(replaceString3);
+                Item.inventory.remove("ELECTRICWIRE");
                 break;
             case "LETTER":
-                u = "You openen the letter and it says: 'Instructions for good coffee: take some fresh water and aromatic coffee beans.'";
+                u = "You opened the letter and it says: 'Instructions for good coffee: take some fresh water and aromatic coffee beans.'";
                 break;
             case "WATER":
                 u = "You put water in the coffee machine";
+                Juuso.coffeeMachineStatus = Juuso.coffeeMachineStatus + 1;
+                Item.inventory.remove("WATER");
                 break;
             default:
                 u = "You cannot use this item here.";
@@ -70,8 +82,8 @@ public class UserInterface {
         switch (esine) {
             case "KNIFE":
                 e = "knife";
-                String s1=Main.building.get(0).getDescription();
-                String replaceString1=s1.replace(" A knife is lying on the floor.","");
+                String s1 = Main.building.get(0).getDescription();
+                String replaceString1 = s1.replace(" A knife is lying on the floor.", "");
                 Main.building.get(0).setDescription(replaceString1);
                 break;
             case "KEY":
@@ -79,20 +91,20 @@ public class UserInterface {
                 break;
             case "COFFEE":
                 e = "coffee";
-                String s2=Main.building.get(0).getDescription();
-                String replaceString2=s2.replace("A bag of freshly ground coffee is placed on one of the dining tables.","");
+                String s2 = Main.building.get(0).getDescription();
+                String replaceString2 = s2.replace("A bag of freshly ground coffee is placed on one of the dining tables.", "");
                 Main.building.get(0).setDescription(replaceString2);
                 break;
             case "WIRE":
                 e = "wire";
-                String s4=Main.building.get(3).getDescription();
-                String replaceString4=s4.replace("The wire to the coffee maker lies on the table the middle of the room.","");
+                String s4 = Main.building.get(3).getDescription();
+                String replaceString4 = s4.replace("The wire to the coffee maker lies on the table the middle of the room.", "");
                 Main.building.get(3).setDescription(replaceString4);
                 break;
             case "LETTER":
                 e = "letter";
-                String s3=Main.building.get(1).getDescription();
-                String replaceString3=s3.replace("A letter is placed on the reception desk.","");
+                String s3 = Main.building.get(1).getDescription();
+                String replaceString3 = s3.replace("A letter is placed on the reception desk.", "");
                 Main.building.get(1).setDescription(replaceString3);
                 break;
             case "WATER":
